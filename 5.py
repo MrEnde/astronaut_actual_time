6ebe677cbf1001235b2a5ac699c84415e3de01f2
MrEnde <mrendor.q@gmail.com>
2024-03-27 17:10:29 +0300

    исправление ошибок 3

--- 5.py
+++ 5.py
@@ -1,42 +1,4 @@
 from csv import DictReader
-from datetime import time
-
-
-def parse_time(row_string: str) -> time:
-    """
-    Парсинг даты формата: "hh:mm:ss"
-
-    row_string - изначальный вид
-    """
-    hours, minutes, seconds = row_string.split(":")
-
-    return time(int(hours), int(minutes), int(seconds))
-
-
-def date_format(hours: int, minutes: int, seconds: int) -> str:
-    """
-    Функция для приведения даты в строковый формат
-    Формат: hh:mm:ss
-
-    hours - часы
-    minutes - минуты
-    seconds - секунды
-    """
-    return f"{str(hours).zfill(2)}:{str(minutes).zfill(2)}:{str(seconds).zfill(2)}"
-
-
-def add_time_now(stations):
-    """
-    Добавляет в модели stations актуальное время
-
-    stations - станции, которые не имеют актуального времени
-    """
-    for station in stations:
-        last = parse_time(station['timeStop'])
-
-        actual_time = date_format((last.hour + int(station['count'])) % 24, last.minute, last.second)
-
-        station.setdefault('timeNow', actual_time)
 
 
 with open("astronaut_time.txt", encoding="utf-8") as file:
@@ -44,7 +6,6 @@ with open("astronaut_time.txt", encoding="utf-8") as file:
     headers = ["WatchNumber", "numberStation", "cabinNumber", "timeStop", "count"]
     stations = list(DictReader(file, delimiter=">", fieldnames=headers))
 
-add_time_now(stations)
 
 table = {}
 for station in stations:
